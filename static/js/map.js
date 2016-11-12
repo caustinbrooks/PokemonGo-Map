1b06d9ca902dae7e229dc65900bc778d96890fe4
mew905 <mew905@gmail.com>
2016-11-12 13:21:06 -0600

    text fix
    if (valid === 1) showed "Disappear time unknown" all the time. Changing to if (valid = 1) restored functionality.

--- static/js/map.js
+++ static/js/map.js
@@ -365,7 +365,7 @@ function pokemonLabel (name, rarity, types, disappearTime, id, latitude, longitu
 
   var dspawn = ''
 
-  if (valid === 1) {
+  if (valid = 1) {
     dspawn = `<div>Disappears at ${pad(disappearDate.getHours())}:${pad(disappearDate.getMinutes())}:${pad(disappearDate.getSeconds())}
     <span class='label-countdown' disappears-at='${disappearTime}'>(00m00s)</span></div>`
   } else {
